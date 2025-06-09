790b7375dd09a429125c584d5808f87af961ad2b
emanuellar2025 <r.emanuella@escola.pr.gov.br>
2025-06-09 11:28:38 -0300

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+const frases = [
+    "O único modo de fazer um excelente trabalho é amar o que você faz.",
+    "Acredite em si mesmo e todo o resto virá naturalmente.",
+    "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
+    "Nunca é tarde demais para ser o que você poderia ter sido.",
+    "A persistência é o caminho do êxito."
+];
+
+function novaFrase() {
+    const indiceAleatorio = Math.floor(Math.random() * frases.length);
+    document.getElementById("frase").textContent = frases[indiceAleatorio];
+}
+
+// Carregar uma frase inicial ao carregar a página
+novaFrase(NÃO IMPORTA O
+QUANTO VOCÊ
+VÁ DEVAGAR
+DESDE QUE
+NÃO PARE);
